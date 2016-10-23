afb956c23c3652edf33fd869565b5b4bc2a9c4de
Cristii5 <cristian.mocanu@lse.org.ro>
2016-10-23 20:02:35 +0300

    Update functieE3.m

--- functieE3.m
+++ functieE3.m
@@ -1,6 +1,7 @@
-function [a,b,c]=functieE3(v)
-a=mean(real(v))
-b=v.^2
-c=v*v'
+function [a,b,c]=functieE3(v) % declararea functiei 
+ % declararea parametrilor de iesire 
+a=mean(real(v)) % media elementelor reale din vector
+b=v.^2 % elementele initiale ridicate la patrat 
+c=v*v' % produsul dintre vectorul initial si transpusa lui
 end
 
