ddb727b6f9963450aaa441bae4e734f0fab16382
Cristii5 <cristian.mocanu@lse.org.ro>
2016-10-23 15:18:53 +0300

    Update ExercitiuE2.m

--- ExercitiuE2.m
+++ ExercitiuE2.m
@@ -1,3 +1,4 @@
+cd % programul este salvat in directorul precizat
 v=randn(1,10) % se genereaza un vector linie de elemente numere aleatoare cu distribu?ie normal?
 for i=1:1:10 % selectam un index care sa se plimbe prin vectorul v
     if v(i)<0 % punem conditia ca acesta sa fie negativ
