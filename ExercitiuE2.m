615e52bf3c18906cadbd7f150472ef463ccd3f8e
Cristii5 <cristian.mocanu@lse.org.ro>
2016-10-23 15:01:34 +0300

    Add files via upload

---
+++ ExercitiuE2.m
@@ -0,0 +1,7 @@
+v=randn(1,10) % se genereaza un vector linie de elemente numere aleatoare cu distribu?ie normal?
+for i=1:1:10 % selectam un index care sa se plimbe prin vectorul v
+    if v(i)<0 % punem conditia ca acesta sa fie negativ
+        v(i) % afisam numerele negative
+    end
+end    
+        
